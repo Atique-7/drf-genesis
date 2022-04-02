a1e64d8df8cd87eec0e261c8b6f97fb10a7cf8c0
Atique-7 <khanatique0000@gmail.com>
2022-04-02 21:37:28 +0530

    Added default userModel

--- main/settings.py
+++ main/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#Overriding the default user model.
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profile"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user"""
+        if not email:
+            raise ValueError("User must have an email.")
+
+        #normalize the email
+        email = self.normalize_email(email)
+
+        #create a user form model
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+
+        user.save(using = self._db)
+
+        return user
+
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new user with given details"""
+
+        user = self.create_user(email, name, password)
+        #not specified in user-profile but is set to false by the permissionsmixin 
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self._db)
+
+        return user
+
+
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """ database model for users in the system """
+    email = models.EmailField(length=255, unique=True)
+    name = models.CharField(length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELD = ["name"]
+
+    def get_full_name(self):
+        """ retrieves full name of user """
+        return self.name
+
+
+    def get_short_name(self):
+        """ retrieves short name of user """
+        return self.name
+
+    def __str__(self):
+        """ returns string representation of our user """
+        return self.email
